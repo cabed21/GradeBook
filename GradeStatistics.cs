81c8c0f04d26822d5e3748c2921cea984c49aa06
Chris Barcia <cabed21@gmail.com>
2018-04-03 15:41:45 -0400

    GradeBook files

---
+++ GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Grades
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
